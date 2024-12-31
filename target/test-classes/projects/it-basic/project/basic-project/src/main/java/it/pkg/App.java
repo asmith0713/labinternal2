42e7e8d96b03a05cbf17452a40a6fbb6e3ff0041
unknown <asmithreddy2004@gmail.com>
2024-12-31 11:01:23 +0530

    first commit

---
+++ target/test-classes/projects/it-basic/project/basic-project/src/main/java/it/pkg/App.java
@@ -0,0 +1,13 @@
+package it.pkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "THIS IS SE LAB INTERNAL 1 EXAM,THE CREATED PROJECT IS OF SREEJA" );
+    }
+}
