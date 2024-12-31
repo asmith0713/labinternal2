a31ad37ef828d646957257e710948d4a758ca430
Asmith Maramreddy <asmithreddy2004@gmail.com>
2024-12-31 11:26:56 +0530

    Update App.java

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "THIS IS SE LAB INTERNAL 1 EXAM,THE CREATED PROJECT IS OF SREEJA" );
+        System.out.println( "lab internal 2" );
     }
 }
